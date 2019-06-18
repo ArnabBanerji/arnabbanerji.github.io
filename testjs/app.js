b8005a2850c857af0b49fdd8855ab9f73140f573
Arnab Banerji <abanerji@deloitte.com>
2019-06-18 14:11:12 +0530

    [Init GCP Test]

--- testjs/app.js
+++ testjs/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
   $('h1').html('Arnab Banerji');
   $('h2').html('Testing search elements - Intekhab.');
+
+  var ds = moment();
+  ds.subtract(17, 'hours');
+  ds.tz("America/New_York").format('en');
+  var newDate = new Date(ds);
+  var userHours = newDate.getHours();
+  var userMinsEST = (userHours * 60) + newDate.getMinutes();
+
+  $('h3').html(userMinsEST);
+
+
 });
